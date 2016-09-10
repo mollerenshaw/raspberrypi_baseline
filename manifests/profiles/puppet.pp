cb91335cec82f38e9229e6f441e2267245093011
Mark <mollerenshaw@zepler.net>
2016-09-10 18:07:38 +0100

    Now manages Hiera.

--- manifests/profiles/puppet.pp
+++ manifests/profiles/puppet.pp
@@ -5,14 +5,22 @@ class raspberrypi_baseline::profiles::puppet($version = 'latest') {
     ensure => $version,
   }
 
-  file { '/etc/puppet/puppet.conf':
+  File {
     ensure   => file,
-    content  => template('raspberrypi_baseline/puppet/puppet.conf.erb'),
     owner    => 'root',
     group    => 'root',
     mode     => '0644',
-    require  => Package['puppet'],
+    require  => Package['puppet'],  
   }
+
+  file {
+    '/etc/puppet/puppet.conf':
+      content  => template('raspberrypi_baseline/puppet/puppet.conf.erb');
+
+    '/etc/puppet/hiera.yaml': 
+      content  => template('raspberrypi_baseline/puppet/hiera.yaml.erb'),
+  }
+
 
   service { 'puppet':
     ensure  => stopped,
