59dd98073b4f573a62b90cf0f811b5f28af5e706
Mark <mollerenshaw@zepler.net>
2016-09-12 18:53:50 +0100

    Added a bit of Docker config.

--- manifests/profiles/docker.pp
+++ manifests/profiles/docker.pp
@@ -1,8 +1,29 @@
 
 class raspberrypi_baseline::profiles::docker($version = 'latest') {
 
-  package { 'docker' :
+  package { ['apt-transport-https', 'ca-certificates'] :
+    ensure => latest,
+    before => Package['docker'],
+  }
+
+  $distro_codename = $facts['os']['lsb']['distcodename']
+
+  file { '/etc/apt/sources.list.d/docker.list' :
+    ensure  => file,
+    content => template('raspberrypi_baseline/docker/docker.list.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    before  => Package['docker'],
+  }
+
+  package { 'docker-engine' :
     ensure => $version,
   }
 
+  service { 'docker' :
+    ensure    => running,
+    enable    => true,
+    subscribe => Package['docker-engine'],
+  }
 }
