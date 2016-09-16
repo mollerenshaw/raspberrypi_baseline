4eca6948b7a71d9f2150c2e63d197fffb8a58c6f
Mark <mollerenshaw@zepler.net>
2016-09-16 17:20:41 +0100

    Added some Samba config.

--- manifests/profiles/samba.pp
+++ manifests/profiles/samba.pp
@@ -5,4 +5,12 @@ class raspberrypi_baseline::profiles::samba {
     ensure => latest,
   }
 
+  file { '/etc/samba/smb.conf' :
+    ensure  => file,
+    content => template('raspberrypi_baseline/samba/smb.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
 }
