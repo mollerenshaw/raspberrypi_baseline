9c9b17037b2db906ee04473138b85a4065635dcd
Mark <mollerenshaw@zepler.net>
2016-09-16 16:45:50 +0100

    Stop doing Docker for the moment.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class raspberrypi_baseline() {
 
   contain '::raspberrypi_baseline::profiles::samba'
   contain '::raspberrypi_baseline::profiles::vim'
-  contain '::raspberrypi_baseline::profiles::docker'
+  #contain '::raspberrypi_baseline::profiles::docker'
   contain '::raspberrypi_baseline::profiles::puppet'
   contain '::raspberrypi_baseline::profiles::git'
 
