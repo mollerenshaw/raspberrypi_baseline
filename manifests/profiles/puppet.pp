0266b5a7211aa351197621dff5a2c4192f3f59dd
Mark <mollerenshaw@zepler.net>
2016-09-17 16:05:45 +0100

    Improved Puppet APT install configuration

--- manifests/profiles/puppet.pp
+++ manifests/profiles/puppet.pp
@@ -1,27 +1,41 @@
-
+#
+# Puppet configuration for my Raspberry Pi.
+#
+# ## Parameters
+#
+# version: The version of Puppet to install.
+#
 class raspberrypi_baseline::profiles::puppet($version = 'latest') {
-	
-  package { 'puppet':
+
+  file { '/etc/apt/sources.list.d/puppet.list':
+    ensure => file,
+    before => Package['puppet-agent']
+  }
+
+  # We'll use Puppet-Agent, which is the name used for Puppet > 4
+  package { 'puppet-agent':
     ensure => $version,
   }
 
+  # Default configuration for files.
   File {
     ensure   => file,
     owner    => 'root',
     group    => 'root',
     mode     => '0644',
-    require  => Package['puppet'],  
+    require  => Package['puppet-agent'],  
   }
 
+  # Create some config files.
   file {
-    '/etc/puppet/puppet.conf':
+    $::settings::config:
       content  => template('raspberrypi_baseline/puppet/puppet.conf.erb');
 
-    '/etc/puppet/hiera.yaml': 
+    $::settings::hiera_config: 
       content  => template('raspberrypi_baseline/puppet/hiera.yaml.erb'),
   }
 
-
+  # We're not using the Puppet Agent daemon (running master-less).
   service { 'puppet':
     ensure  => stopped,
     enable  => false,
