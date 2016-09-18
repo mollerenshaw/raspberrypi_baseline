dada867ff3a11fc1cda666b4c35410e37f00c052
Mark <mollerenshaw@zepler.net>
2016-09-18 20:54:59 +0100

    Stop trying to install puppet for the moment.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,12 @@
 #
 class raspberrypi_baseline() {
 
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+  }
+
+  Exec["apt-update"] -> Package <| |>
+
   # Use this to mount the Raspberry Pi's disk over the network.
   contain '::raspberrypi_baseline::profiles::samba'
 
@@ -10,7 +16,7 @@ class raspberrypi_baseline() {
   contain '::raspberrypi_baseline::profiles::vim'
 
   # Deployment/config control tools.
-  contain '::raspberrypi_baseline::profiles::puppet'
+  #contain '::raspberrypi_baseline::profiles::puppet'
   #contain '::raspberrypi_baseline::profiles::docker'
 
   # Some source control tools.
