b03e59eb4921bb2d7410b49455970a676a0b06c4
Mark <mollerenshaw@zepler.net>
2016-09-18 20:52:40 +0100

    Added some comments to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,17 @@
-
+#
+#
+#
 class raspberrypi_baseline() {
 
+  # Use this to mount the Raspberry Pi's disk over the network.
   contain '::raspberrypi_baseline::profiles::samba'
+
+  # Editor config.
   contain '::raspberrypi_baseline::profiles::vim'
-  #contain '::raspberrypi_baseline::profiles::docker'
+
+  # Deployment/config control tools.
   contain '::raspberrypi_baseline::profiles::puppet'
+  #contain '::raspberrypi_baseline::profiles::docker'
 
   # Some source control tools.
   contain '::raspberrypi_baseline::profiles::git'
